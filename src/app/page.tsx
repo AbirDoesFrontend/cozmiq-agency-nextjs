c17a17935c10b6c53b93cdc6c6692dd4616ce8ec
abirdoesfrontend <abirkhan.official.tsx@gmail.com>
2025-08-11 18:44:01 +0600

    Remove unused tag images and replace the Hero section in the homepage with a dedicated Hero component for improved structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import Hero from "@/components/home/Hero";
+
 export default function Home() {
   return (
     <>
-      <section>
-        <h2>Hero Section</h2>
-      </section>
+      <Hero />
     </>
   );
 }
