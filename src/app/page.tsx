b23b631b789fefed33e162e1254ba7ca623779a2
abirdoesfrontend <abirkhan.official.tsx@gmail.com>
2025-08-13 19:02:11 +0600

    Update: Stats Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/home/about/About";
 import Hero from "@/components/home/hero/Hero";
 import Project from "@/components/home/project/Project";
+import StatsSection from "@/components/home/stats/StatsSection";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <About />
       <Project />
+      <StatsSection />
     </>
   );
 }
