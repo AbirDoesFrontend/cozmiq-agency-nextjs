8f8871276c7a43c473c8a4e1bdba56a550e23ef2
abirdoesfrontend <abirkhan.official.tsx@gmail.com>
2025-08-14 16:36:12 +0600

    Add Pricing component to homepage layout for enhanced service offerings presentation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/home/about/About";
 import Discovery from "@/components/home/discovery/Discovery";
 import Hero from "@/components/home/hero/Hero";
+import Pricing from "@/components/home/pricing/Pricing";
 import Project from "@/components/home/project/Project";
 import ServicesSection from "@/components/home/services/ServicesSection";
 import StatsSection from "@/components/home/stats/StatsSection";
@@ -19,6 +20,7 @@ export default function Home() {
       <HowWeWork />
       <Discovery />
       <WhyChooseUs />
+      <Pricing />
     </>
   );
 }
